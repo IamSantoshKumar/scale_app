c31b1b4e2d25f9ca1010328a600c983ec03869e5
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-20 09:59:09 +0530

    second commint

--- main.tf
+++ main.tf
@@ -1,31 +1,6 @@
-provider "aws" {
-  region = "eu-north-1"
-}
-
-# Reuse or create VPC
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
-
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-north-1a", "eu-north-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-# EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "21.0.0"
 
   cluster_name                   = "my-eks-cluster"
   cluster_version                = "1.29"
